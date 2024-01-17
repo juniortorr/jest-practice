a67fac55b574ca3c1a8bc34e39d2f661d74692df
juniortorr <juniortorr77@gmail.com>
2024-01-16 23:20:13 -0800

    - add min and max tests - add getMin & getMax fn

--- helpers/analyze.js
+++ helpers/analyze.js
@@ -8,4 +8,22 @@ const getAverage = (arr, obj) => {
   return obj;
 };
 
-export { getAverage };
+const getMin = (arr, obj) => {
+  const min = arr.reduce((a, b) => {
+    if (a < b) return a;
+    else return b;
+  });
+  obj.min = min;
+  return obj;
+};
+
+const getMax = (arr, obj) => {
+  const max = arr.reduce((a, b) => {
+    if (a < b) return b;
+    else return a;
+  });
+  obj.max = max;
+  return obj;
+};
+
+export { getAverage, getMin, getMax };
--- tests/analyze.test.js
+++ tests/analyze.test.js
@@ -1,7 +1,19 @@
-import { getAverage } from '../helpers/analyze';
+import { getAverage, getMin, getMax } from '../helpers/analyze';
 
 test('return average key value pair', () => {
   const arr = [5, 10, 15];
   const object = {};
   expect(getAverage(arr, object)).toEqual({ average: 10 });
+});
+
+test('return min', () => {
+  const arr = [5, 10, 15];
+  const obj = {};
+  expect(getMin(arr, obj)).toEqual({ min: 5 });
+});
+
+test('return max', () => {
+  const arr = [5, 10, 15];
+  const obj = {};
+  expect(getMax(arr, obj)).toEqual({ max: 5 });
 });
